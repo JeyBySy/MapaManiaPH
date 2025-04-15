9f27234663ff155d857c8e87f94f0ff15d0d819d
JeyBySy <njcocosa@gmail.com>
2025-04-16 01:04:10 +0800

    Add Footer component with attribution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Footer: React.FC = () => {
+    return (
+        <div className="py-10 flex items-center justify-between text-sm text-gray-500 gap-2">
+            <span>Made with 💙 by <a target='_blank' href="https://github.com/JeyBySy">JeyBySy</a></span>
+        </div>
+    )
+}
+
+export default Footer
