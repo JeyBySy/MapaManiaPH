64aed5fbfb5a9a14e9e53c8cf4d9459611fe6645
JeyBySy <njcocosa@gmail.com>
2025-04-16 01:04:18 +0800

    Add NotFound page with 404 message and footer

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer"
+
+const NotFound = () => {
+    return (
+        <>
+            <div className="min-h-[80vh] flex flex-col items-center justify-center">
+                <h1>404 - Page Not Found</h1>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default NotFound
