dcdcdde55a31a23bdcb60e1050b479e8ac6fcf5a
JeyBySy <njcocosa@gmail.com>
2025-04-16 01:02:57 +0800

    Test interaction of SVG

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import StartPage from './pages/StartPage'
+import NotFound from './pages/NotFound'
+import ExploreMapPage from './pages/Modes/ExploreMapPage'
+import FullScreenLayout from './layouts/FullScreenLayout'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <Routes>
+            <Route index path="/" element={<StartPage />} />
+
+            <Route element={<FullScreenLayout />}>
+              <Route path="/exploremap" element={<ExploreMapPage />} />
+            </Route>
+            {/* Catch-all route for 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+
+        </div>
+      </Router>
     </>
   )
 }
