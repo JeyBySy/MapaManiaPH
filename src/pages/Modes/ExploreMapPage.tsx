21f833ef0cf05c9ae3937fd6664ef6a9eff66a25
JeyBySy <njcocosa@gmail.com>
2025-04-25 04:50:16 +0800

    Update UI for explore page

--- src/pages/Modes/ExploreMapPage.tsx
+++ src/pages/Modes/ExploreMapPage.tsx
@@ -4,73 +4,50 @@ import { useProvince } from "../../hooks/useProvince"
 import MapSVG from "../../components/MapSVG"
 
 const ExploreMapPage: React.FC = () => {
-  const { provinceOutline, provinceKeys, selectProvince } = useProvince(false)
+  const { provinceOutline, provinceKeys, selectProvince } = useProvince()
 
   const handleClickProvince = (province: string) => {
     selectProvince(province)
   }
 
   return (
-    <div className="flex h-screen w-full">
-      {/* Left side: Province List */}
-      <aside className="w-1/6 border-dashed border-r-4 overflow-y-auto ">
-        <h2 className="text-xl font-semibold mb-4 bg-gray-800 p-5">
-          Provinces
-        </h2>
-        <div className="flex flex-col">
-          <ul className="text-xs flex flex-col gap-1 py-2 bg-gray-700">
-            {provinceKeys.map((province) => (
-              <li
-                key={province}
-                onClick={() => {
-                  handleClickProvince(province)
-                }}
-                className={`hover:bg-gray-500 cursor-pointer p-3 capitalize ${provinceOutline === province ? "bg-gray-500" : ""
-                  }`}
-              >
-                {province.replace(/_/g, " ").toLowerCase()}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </aside>
-
-      {/* Right side: SVG Map Container */}
-      <main className="flex-1 flex items-center justify-center">
-        <div className="w-full h-full p-4 border">
+    <div className="flex min-h-screen w-full">
+      <main className="flex lg:flex-col-reverse flex-col items-center justify-between w-full h-full p-4">
+        {/* Province outline / Map Display */}
+        <div className="w-full h-[85vh] md:h-[85vh] lg:border-none overflow-hidden border">
           {provinceOutline && LGU_PATHS[provinceOutline] ? (
-            // <svg
-            //   xmlns="http://www.w3.org/2000/svg"
-            //   width="100%"
-            //   height="100%"
-            //   viewBox={LGU_PATHS[provinceOutline]?.viewBox || "0 0 100 100"}
-            // >
-            //   {LGU_PATHS[provinceOutline]?.paths.map((path, index) => (
-            //     <path
-            //       key={path.id || index}
-            //       id={path.id || undefined}
-            //       d={path.d}
-            //       fill="auto"
-            //       className={`map_svg fill-gray-50 dark:fill-gray-200/90 hover:fill-green-400`}
-            //       stroke="black"
-            //       onClick={(e) => {
-            //         e.preventDefault()
-            //         console.log(`Clicked on ${path.id || index}`)
-            //       }}
-            //     />
-            //   ))}
-            // </svg>
             <MapSVG
               provinceName={provinceOutline}
               pathsData={LGU_PATHS}
               mode="explore"
               onPathClick={(id) => console.log(`Clicked ${id}`)}
             />
-
-
           ) : (
-            `${provinceOutline} was not found in LGU_PATHS`
+            <p className="text-center p-4">{provinceOutline} was not found in LGU_PATHS</p>
           )}
+        </div>
+
+        {/* Province Dropdown */}
+        <div className="relative z-10 w-full max-w-xs md:max-w-sm my-4 md:mt-0 rounded mx-auto ">
+          <select
+            name="province"
+            id="provinceID"
+            className="w-full dark:bg-green-600/50 bg-green-600 p-3 text-sm rounded-md max-h-60 text-center shadow"
+            onChange={(e) => handleClickProvince(e.target.value)}
+            defaultValue=""
+          >
+            {provinceKeys.map((province) => (
+              <option
+                key={province}
+                value={province.replace(/_/g, " ")}
+                onClick={() => {
+                  handleClickProvince(province)
+                }}>
+                {province.replace(/_/g, " ")}
+              </option>
+            ))}
+
+          </select>
         </div>
       </main>
     </div>
