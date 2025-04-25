342203f747d6046e283db467e51bb942334c4763
JeyBySy <njcocosa@gmail.com>
2025-04-25 16:02:25 +0800

    Replace Link component with useNavigate for HomeButton navigation

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,14 +1,16 @@
 import { House } from 'lucide-react'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
 
 const HomeButton: React.FC = () => {
+    const navigate = useNavigate()
+    const handleBackButton = () => {
+        navigate(`/`)
+    }
     return (
-        <Link to={'/'}>
-            <button className='hidden lg:block absolute top-2 right-2 p-3 cursor-pointer hover:bg-gray-200 bg-gray-200/50 rounded hover:text-gray-500 shadow-2xl '><House /></button>
-        </Link>
+        <button onClick={() => { handleBackButton() }} className='hidden lg:block absolute top-2 right-2 p-3 cursor-pointer hover:bg-gray-200 bg-gray-200/50 rounded hover:text-gray-500 shadow-2xl '><House /></button>
     )
 }
 
