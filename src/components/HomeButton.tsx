57d0c840f10b6b3e10708890922bd0dc77488267
JeyBySy <njcocosa@gmail.com>
2025-04-27 17:19:55 +0800

    Fix HomeButton styling by adding border and adjusting padding for better visibility

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -10,10 +10,10 @@ const HomeButton: React.FC<HomeButtonProps> = ({ titlePage }) => {
     const navigate = useNavigate();
 
     return (
-        <div className="w-full lg:absolute h-fit flex items-center gap-3 px-4">
+        <div className="w-full lg:absolute h-fit flex items-center gap-3 lg:px-4">
             <button
                 onClick={() => navigate(-1)}
-                className="w-fit items-center  p-4 gap-1 text-white/80 hover:text-white dark:text-gray-300/80 dark:hover:text-gray-300 cursor-pointer"
+                className="w-fit items-center border p-4 gap-1 text-white/80 hover:text-white dark:text-gray-300/80 dark:hover:text-gray-300 cursor-pointer"
             >
                 <ArrowLeft width={25} height={25} />
             </button>
